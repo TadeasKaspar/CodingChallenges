bcabee82d9718af32da4d5af82846b60b0b6b328
TadeasKaspar <tadeas.kaspar@gmail.com>
2022-01-04 21:46:56 +0700

    Coding challenge and solution from 1-4-2022

---
+++ CC 1-4-2022/app.js
@@ -0,0 +1,16 @@
+// Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+// Examples:
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+
+// a = "abcdefghijklmnopqrstuvwxyz"
+// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+function longest(s1, s2) {
+  let sen = s1 + s2;
+  let uniq = sen.split("");
+  let un = [...new Set(uniq)];
+  return un.sort().join("");
+}
