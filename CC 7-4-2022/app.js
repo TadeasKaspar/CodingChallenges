b8cfa78de1ff2d45d0e54c49ae72da03d1ba7599
TadeasKaspar <tadeas.kaspar@gmail.com>
2022-04-07 13:06:21 +0700

    Coding challenge and solution from 7-4-2022

--- CC 7-4-2022/app.js
+++ CC 7-4-2022/app.js
@@ -7,7 +7,6 @@
 // 21    23    25    27    29
 // ...
 // Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
-
 // 1 -->  1
 // 2 --> 3 + 5 = 8
 
